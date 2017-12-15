fe3d4e59659e0dcb1ff32f24ce0101ea52681e8a
Luke Sneeringer <lukesneeringer@google.com>
2017-12-15 12:54:33 -0800

    More staging script work.

--- .github/stage_autogen_api.py
+++ .github/stage_autogen_api.py
@@ -23,35 +23,78 @@ import re
 import shutil
 import sys
 
+
+# It is important that this script can run with vanilla Python.
+#
+# Colors are non-trivial to do, so this is an 80% solution; if you install
+# click, you get colors.
 try:
     import click
+    _ = click.secho
 except ImportError:
-    print('You need the `click` Python library to run this script.')
-    print('Install it with: `pip install click`.')
-    sys.exit(120)
+    def _(message, file=sys.stdout, nl=True, **kwargs):
+        """Print a message to the console."""
+        if nl:
+            message += '\n'
+        file.write(message)
 
 
 REPO_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 
-@click.command()
-@click.argument('src', type=click.Path(exists=True))
-@click.option('--dest', default=None)
 def stage_api(src, dest):
-    _ = click.secho
+    """Stage an auto-generated API in the appropriate location in g-c-python.
 
-    # Sanity check: Ensure that the source directory looks like an API.
-    if not os.path.isfile(os.path.join(src, 'setup.py')):
-        raise click.BadParameter('Not a Python package: %s' % src)
+    Args:
+        src (str): The file path where the auto-generated API was written
+            to disk.
+        dest (str): Optional. The top-level directory where the API should
+            be staged. This is determined based on the package name in
+            setup.py and generally should not be needed.
 
+    Returns:
+        int: The exit code.
+    """
     # Determine the destination directory, if one was not explicitly provided.
     #
     # We do this by reading setup.py and getting the name of the package from
     # there, and stripping the google-cloud- prefix.
     if dest:
         _('Destination directory explicitly set: \r', nl=False)
-        _('\r%s/' % dest, fg='green', bold=True)
+        _('\r%s/' % dest, fg='green')
     else:
         _('Determining directory to stage in: \r', nl=False)
         with io.open(os.path.join(src, 'setup.py'), 'r') as setup_py:
-            setup_script = setup_py.read()
+            setup = setup_py.read()
+            package_match = re.search(r"name='google-cloud-([a-z-]+)',", setup)
+
+            # If we could not find a package, abort loudly.
+            if not package_match:
+                _('\r ERROR', fg='red', bold=True)
+                _('Could not determine the staging location. Is this a valid ')
+                _('Google Cloud API package? If it is, provide the dest ')
+                _('directory manually.')
+                return 32
+
+            # The dest is the end of the package name; google-cloud prefix
+            # dropped.
+            dest = package_match.groups()[1].replace('-', '_')
+            _('\r%s/' % dest, fg='green')
+
+
+def _verify_src(src):
+    """Verify the source directory."""
+    src = os.path.realpath(os.path.expanduser(src))
+    if not os.path.isdir(src):
+        raise argparse.ArgumentTypeError('Path not found: %s' % src)
+    if not os.path.isfile(os.path.join(src, 'setup.py')):
+        raise argparse.ArgumentTypeError('Not a Python package: %s' % src)
+    return src
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Stage an auto-generated API')
+    parser.add_argument('src', type=_verify_src)
+    parser.add_argument('dest', default=None)
+    args = parser.parse_args()
+    sys.exit(stage_api(args.src, args.dest))
